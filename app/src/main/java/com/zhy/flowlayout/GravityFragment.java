dc430ca4de389888f71187950a0dbd8edb8a834a
安卓开发电脑 <18950261460@163.com>
2021-12-30 11:56:16 +0800

    first commit

---
+++ app/src/main/java/com/zhy/flowlayout/GravityFragment.java
@@ -0,0 +1,19 @@
+package com.zhy.flowlayout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class GravityFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_gravity_flow_layout, null);
+    }
+}
